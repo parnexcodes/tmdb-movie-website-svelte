76ba7062e15fb2a455edafc01e298e699499c394
Paranjay Singh <parnexcodes@gmail.com>
2021-12-05 18:08:08 +0530

    add navbar

---
+++ components/Header.svelte
@@ -0,0 +1,14 @@
+<div class="flex mx-8">
+    <li class="list-none my-8">
+        <a href="/" class="rounded-md px-4 py-2 mr-4  text-gray-300 hover:opacity-70">Home</a>
+    </li>
+    <li class="list-none my-8">
+        <a href="/" class="rounded-md px-4 py-2 mr-4  text-gray-300 hover:opacity-70">GitHub</a>
+    </li>
+    <li class="list-none my-8">
+        <a href="/" class="rounded-md px-4 py-2 mr-4  text-gray-300 hover:opacity-70">API</a>
+    </li>
+    <li class="list-none my-8">
+        <a href="/" class="rounded-md px-4 py-2 mr-4  text-gray-300 hover:opacity-70">Contact</a>
+    </li>
+</div>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -2,11 +2,14 @@
     import { onMount } from 'svelte';
     import { element } from 'svelte/internal';
     import Footer from '../../components/Footer.svelte'
+    import Header from '../../components/Header.svelte'
     let api_key = '4c1c4651b470f738873f80310325d848'
     let result = []
+    let page = ''
+    let pageNum = [1,2,3,4,5,6,7,8,9,10]
   
     const apiReq = async () => {
-      const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US`)
+      const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=${page}`)
       result = await response.json()
       result = result['results']
       console.log(result)
@@ -17,6 +20,7 @@
       });
   
   </script>
+  <Header />
   <section>
     <div class="text-center text-white text-5xl my-8">
       <h1>Trending Movies</h1>
@@ -31,7 +35,21 @@
       </div>
       {/each}
       </div>
-  
+
+      <div class="flex justify-center">
+        <li class="flex-row list-none my-8 space-x-4">
+          <a href="/" class="rounded-md px-4 py-2 mr-4  text-gray-300 hover:opacity-70">Previous Page</a>
+        </li>
+      <li class="flex-row list-none my-8 space-x-4">
+        {#each pageNum as pages}
+      <a href="/page/{pages}" class="rounded-md px-4 py-2  text-gray-300 hover:opacity-70">{pages}</a>
+      {/each}
+    </li>
+    <li class="flex-row list-none my-8 space-x-4">
+      <a href="/" class="rounded-md px-4 py-2 ml-4 text-gray-300 hover:opacity-70">Next Page</a>
+    </li>
+  </div>
+
   </section>
 <br>
 
